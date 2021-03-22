e4495719712eaaa40ab08b350c29b438d3309d6b
crazyeights <dataiisthenewcurrency@gmail.com>
2021-03-22 16:51:54 -0400

    added ccsc

---
+++ ccsc/assets/js/calendar.js
@@ -0,0 +1,31 @@
+var calendar = new Calendar('#calendar', {
+    defaultView: 'month',
+    taskView: true,
+    template: {
+      monthDayname: function(dayname) {
+        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+      }
+    }
+  });
+  calendar.createSchedules([
+    {
+        id: '1',
+        calendarId: '1',
+        title: 'my schedule',
+        category: 'time',
+        dueDateClass: '',
+        start: '2018-01-18T22:30:00+09:00',
+        end: '2018-01-19T02:30:00+09:00',
+        isReadOnly: true
+    },
+    {
+        id: '2',
+        calendarId: '1',
+        title: 'second schedule',
+        category: 'time',
+        dueDateClass: '',
+        start: '2018-01-18T17:30:00+09:00',
+        end: '2018-01-19T17:31:00+09:00',
+        isReadOnly: true    // schedule is read-only
+    }
+]);
